d04b925d788d511ec98af19c08bc77a428abd90c
jenish5688 <jenishpatel.bmc@gmail.com>
2015-05-29 13:48:37 +0530

    Update scripts.js

--- proctree01/js/scripts.js
+++ proctree01/js/scripts.js
@@ -20,21 +20,13 @@ function drawLine(x,y,x1,y1,depth){
 
 function drawTree(x,y,angle,depth){
 	
-	var x1 = x + Math.cos(angle*deg_to_rad)*  depth/Math.PI*(15 / depth + (Math.random()*20)/(depth+3) + 15/depth )  ;
-	var y1 = y + Math.sin(angle*deg_to_rad)* depth/Math.PI*  (15 / depth + (Math.random()*20)/(depth+3) + 15/depth )  ;
+	var x1 = x + Math.cos(angle*deg_to_rad)*  depth/Math.PI //* (15 / depth + (Math.random()*20)/(depth+3) + 15/depth )  ;
+	var y1 = y + Math.sin(angle*deg_to_rad)*  depth/Math.PI //* (15 / depth + (Math.random()*20)/(depth+3) + 15/depth )  ;
 	
 	if(depth != 0 && counter != 1500){
 		drawLine(x,y,x1,y1,depth);
 
-			/*ctx.beginPath();
-		ctx.moveTo(x,y);
-		ctx.lineTo(x1,y1);
-		ctx.stroke();
 		
-			ctx.beginPath();
-		ctx.moveTo(x,y);
-		ctx.lineTo(x1,y1);
-		ctx.stroke();*/
 		drawTree(x1,y1,angle+(10*-Math.random()+5),depth-1);
 		if(Math.ceil(Math.random()*( Math.cbrt( depth)+3))===1){
 			drawTree(x1,y1,angle +(40)/Math.sqrt( Math.sqrt(Math.sqrt( depth))),depth-Math.floor(1+Math.random()*( depth)));
@@ -45,20 +37,7 @@ function drawTree(x,y,angle,depth){
 			drawTree(x1,y1,angle +(-40)/Math.sqrt(Math.sqrt( Math.sqrt( depth))),depth-Math.floor(1+Math.random()*( depth)));
 		}
 		
-	//	if(Math.ceil(Math.random()*3)===1){
-	//		drawTree(x1,y1,angle +(-80*Math.random()+ 40)/Math.sqrt( Math.sqrt(Math.sqrt( depth))),depth-Math.floor(1+Math.random()*depth));
-	//	}
-//		else{
-//			drawTree(x1,y1,angle -10,depth-1);
-//		}
-//		
-//		if(Math.ceil(Math.random()*3)===1){
-//			drawTree(x1,y1,angle -10,depth-1);
-//		}
-//		else{
-//			drawTree(x1,y1,angle +10,depth-1);
-//		}
-//		
+
 	}
 	
 }
